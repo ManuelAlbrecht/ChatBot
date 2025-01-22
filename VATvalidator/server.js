53f740d09ab0d546198fa6b81aaf4a3f4899188a
ManuelAlbrecht <info@manuel-albrecht.com>
2025-01-22 16:21:03 +0530

    Update server.js
    Updated port from 3000 to 6000

--- VATvalidator/server.js
+++ VATvalidator/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { checkVAT } = require('./vatChecker');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 
 app.use(express.json());
 
